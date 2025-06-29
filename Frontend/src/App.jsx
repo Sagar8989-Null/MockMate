37cff18836b350c54e599830f6a5e4f5a3dc386c
Sagar8989-Null <sagarjamadar8989@gmail.com>
2025-06-29 22:48:46 +0530

    shifted hash router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import MockInterview from './components/MockInterview';
 import './App.css';
